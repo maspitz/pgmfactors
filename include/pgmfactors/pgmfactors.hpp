aa6878c8f727a53bde51f231965ceec93c5c9e55
Michael Spitznagel <exultavit@gmail.com>
2023-03-10 17:00:12 -0500

    Define basic random variables and factors

--- include/pgmfactors/pgmfactors.hpp
+++ include/pgmfactors/pgmfactors.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 #include "pgmfactors/pgmfactors_export.hpp"
 
@@ -68,3 +69,41 @@ private:
   PGMFACTORS_SUPPRESS_C4251
   std::string m_name;
 };
+
+
+namespace pgmfactors {
+
+// random_variable identifies a finite valued random variable.
+// It doesn't model probability.  That is handled by Factor.
+class random_variable {
+    const int m_cardinality;
+    const int m_id;
+    const std::string m_name;
+    inline static int m_next_id = 0;
+  public:
+    explicit random_variable(int cardinality) :
+      m_cardinality(cardinality), m_id(++m_next_id), m_name("unnamed rv") {}
+    explicit random_variable(std::string name, int cardinality) :
+      m_cardinality(cardinality), m_id(++m_next_id), m_name(std::move(name)) {}
+    [[nodiscard]] auto id() const noexcept { return m_id; }
+    [[nodiscard]] auto cardinality() const noexcept { return m_cardinality; }
+    [[nodiscard]] auto name() const noexcept { return m_name; }
+};
+
+  // Note: Factor models only discrete factors at this time
+  // Class invariants
+  //   rv_id is in strictly ascending order
+  //   elements of rv_id are not repeated
+  class factor {
+    public:
+      using value_type = double;
+      using variable_list = const std::vector<random_variable>;
+      using data_list = const std::vector<value_type>;
+    private:
+      variable_list rand_vars;
+      data_list data;
+    public:
+      explicit factor(variable_list rand_vars,
+                      data_list data) : rand_vars(rand_vars), data(data) { }
+  };
+}
