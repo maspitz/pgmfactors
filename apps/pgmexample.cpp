8f4b37788dca9bccbf9ac5ca6eecebdf06c25043
Michael Spitznagel <exultavit@gmail.com>
2023-03-24 17:05:13 -0400

    Add sample factors to demonstrate product operation

--- apps/pgmexample.cpp
+++ apps/pgmexample.cpp
@@ -24,6 +24,18 @@ void print_xarray_with_coords(const xt::xarray<T> &xa) {
 
 int main() {
 
+    auto AB_data = xt::xarray<double>{0.5, 0.8, 0.1, 0.0, 0.3, 0.9};
+    AB_data.reshape({3, 2});
+    auto AB_vars = pgmfactors::factor::rv_list{1,2};
+    pgmfactors::factor f_AB(AB_vars,
+                            pgmfactors::factor::data_array(AB_data));;
+
+    auto BC_data = xt::xarray<double>{0.5, 0.7, 0.1, 0.2};
+    BC_data.reshape({2, 2});
+    auto BC_vars = pgmfactors::factor::rv_list{2,3};
+    pgmfactors::factor f_BC(BC_vars,
+                            pgmfactors::factor::data_array(BC_data));;
+
     auto rvlst = pgmfactors::factor::rv_list{1,2};
     auto mydata = pgmfactors::factor::data_array({{1,2},{3,4}});
     pgmfactors::factor f(rvlst, mydata);
