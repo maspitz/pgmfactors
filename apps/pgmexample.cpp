782ad6032e8eb7655de8ace1db670b41087b5e8b
Michael Spitznagel <exultavit@gmail.com>
2023-03-24 16:55:17 -0400

    Implement xarray printing with n-dim indices

--- apps/pgmexample.cpp
+++ apps/pgmexample.cpp
@@ -1,8 +1,33 @@
 // pgmexample.cpp
 #include <iostream>
+#include <xtensor/xstrides.hpp>
+#include <xtensor/xio.hpp>
+#include <xtensor/xarray.hpp>
+#include <xtensor/xtensor.hpp>
+#include <xtensor/xaxis_iterator.hpp>
+#include <xtensor/xaxis_slice_iterator.hpp>
 #include "pgmfactors/pgmfactors.hpp"
 
+template<typename T>
+void print_xarray_with_coords(const xt::xarray<T> &xa) {
+    size_t flat_index{0};
+    for(auto val : xa) {
+        auto idx_vector = xt::unravel_index(flat_index, xa.shape());
+        for(auto idx_coord : idx_vector) {
+            std::cout << idx_coord << " ";
+        }
+        std::cout << val << std::endl;
+        flat_index++;
+    }
+}
+
+
 int main() {
-    std::cout << "Hi world!" << std::endl;
+
+    auto rvlst = pgmfactors::factor::rv_list{1,2};
+    auto mydata = pgmfactors::factor::data_array({{1,2},{3,4}});
+    pgmfactors::factor f(rvlst, mydata);
+
+    print_xarray_with_coords(f.data());
     return 0;
 }
