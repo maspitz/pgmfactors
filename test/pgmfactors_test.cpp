7951b046468376edc0fe26566cbe9c5f7cda86a3
Michael Spitznagel <exultavit@gmail.com>
2023-03-27 16:33:37 -0400

    Migrate Catch2 from v2 to v3

--- test/pgmfactors_test.cpp
+++ test/pgmfactors_test.cpp
@@ -1,8 +1,7 @@
 // pgmfactors_test.cpp
 
 #include "pgmfactors/pgmfactors.hpp"
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
+#include <catch2/catch_test_macros.hpp>
 
 
 TEST_CASE("Factor Product", "[pgmfactors]")
