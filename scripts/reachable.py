27af841bbd846eb765344d39fa0b4892741f69f0
Michael Spitznagel <exultavit@gmail.com>
2023-04-07 17:01:57 -0400

    Create Digraph class to represent Bayes Nets

--- scripts/reachable.py
+++ scripts/reachable.py
@@ -7,6 +7,40 @@ def reachable(G, X, Z):
     G: Graph of Bayes Net
     X: Source Random Variable
     Z: Observation Random Variables
+
+class Digraph:
+    """A simple directed graph."""
+    def __init__(self):
+        self.nodes = set()
+        self.forward_edges = dict()
+        self.reverse_edges = dict()
+        self.edges = self.forward_edges
+
+    def add(self, node, children):
+        """Adds a node and its outward edges to the graph.
+
+        node: Identifies the node.  Must be hashable.
+
+        children: An iterable of nodes corresponding to the outward edges.  Must also be hashable."""
+
+        new_nodes = set.union({node}, children) - self.nodes
+        self.nodes |= new_nodes
+        for n in new_nodes:
+            self.forward_edges[n] = set()
+            self.reverse_edges[n] = set()
+
+        self.forward_edges[node].update(children)
+        for child in children:
+            self.reverse_edges[child].add(node)
+
+    def parents(self, node):
+        """Returns the parents of the node."""
+        return self.reverse_edges[node]
+
+    def children(self, node):
+        """Returns the children of the node."""
+        return self.forward_edges[node]
+
     """
 
     pass
