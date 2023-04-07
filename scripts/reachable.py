1b81a8099e6007ff4e6a4f9257ef17667e996b3e
Michael Spitznagel <exultavit@gmail.com>
2023-04-07 15:24:46 -0400

    Create placeholder for reachable-nodes algorithm
    This is an algorithm for finding nodes that are reachable via
    active trails in a given Bayes Net.  It will be implemented in Python.

---
+++ scripts/reachable.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+def reachable(G, X, Z):
+    """Returns set of nodes reachable from X given Z via active trails, for Bayes Net G.
+
+    G: Graph of Bayes Net
+    X: Source Random Variable
+    Z: Observation Random Variables
+    """
+
+    pass
